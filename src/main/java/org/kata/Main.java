41e1ea24769888db4e23ebb55d6c75c8bb5a156c
KATAs <maryam.yaghoobi@rocketmail.com>
2025-04-19 19:41:54 +0430

    REFACTOR: Final code

--- src/main/java/org/kata/Main.java
+++ src/main/java/org/kata/Main.java
@@ -1,7 +1,29 @@
 package org.kata;
 
+import java.util.Scanner;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        PrimeNumberIdentifier checker = new PrimeNumberIdentifier();
+
+        while (true) {
+            System.out.print("لطفاً یک عدد وارد کنید : ");
+            String input = scanner.nextLine();
+            try {
+                int number = Integer.parseInt(input);
+                if (checker.isPrime(number)) {
+                    System.out.println(number + " یک عدد اول است.");
+                } else {
+                    System.out.println(number + " عدد اول نیست.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("ورودی نامعتبر! لطفاً یک عدد صحیح وارد کنید.");
+            }
+
+            System.out.println(); // خط خالی برای جداسازی
+        }
+
     }
 }
