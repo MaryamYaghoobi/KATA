2b11446f07ac0a9daacdc5fb01bc9a0d79d6d350
KATAs <maryam.yaghoobi@rocketmail.com>
2025-04-19 19:13:16 +0430

    GREEN: Implement Calculator

--- src/main/java/org/kata/PrimeNumberIdentifierTest.java
+++ src/main/java/org/kata/PrimeNumberIdentifierTest.java
@@ -2,9 +2,24 @@ package org.kata;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class PrimeNumberIdentifierTest {
+
     @Test
-    public void testIsPrime() {
+    public void testNonPrimeNumbers() {
+        PrimeNumberIdentifier checker = new PrimeNumberIdentifier();
+        assertFalse(checker.isPrime(1));
+        assertFalse(checker.isPrime(4));
+        assertFalse(checker.isPrime(9));
+        assertFalse(checker.isPrime(15));
+    }
+
+    @Test
+    public void testPrimeNumbers() {
         PrimeNumberIdentifier checker = new PrimeNumberIdentifier();
         assertTrue(checker.isPrime(2));
+        assertTrue(checker.isPrime(3));
+        assertTrue(checker.isPrime(5));
+        assertTrue(checker.isPrime(7));
+        assertTrue(checker.isPrime(11));
+        assertTrue(checker.isPrime(13));
     }
 }
